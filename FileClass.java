c398deb1db8609a0ef0e1093cae9bbaa8d79de11
swathyrajiv <71811725+swathyrajiv@users.noreply.github.com>
2020-09-30 20:05:20 +0530

    newfile with search update

--- FileClass.java
+++ FileClass.java
@@ -1,12 +1,12 @@
 package file_handling;
 
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 	import java.io.IOException;
@@ -14,10 +14,13 @@ import java.io.FileWriter;
 
 	public class  FileClass {
 
-		public static void main(String[] args) {
+		public static void main(String[] args)  {
+
 			Scanner scanner = new Scanner(System.in);
-			int   c;
-			System.out.println("Welcome to FMS 1.0");
+		
+			int  c = 0;
+			System.out.println("\tWelcome to FMS 1.0");
+			System.out.println("developed by simplylearn.\tquality is our priority\n");
 			
 			do {
 				System.out.println("press 1 for file insertion");
@@ -26,29 +29,61 @@ import java.io.FileWriter;
 				System.out.println("press 4 for file searching");
 				System.out.println("press 0 to exit the application ");
 				System.out.println("press 5 to continue");
-				c= scanner.nextInt();
+					try {
+					c =scanner.nextInt();
+				
+					}catch (InputMismatchException e) {
+						System.out.println("please enter a valid input");
+					}
+					
+								
 				switch (c) {
-				case 1 : System.out.println("enter the name of file to be created");	
-				String filename =scanner.next() ;
+				case 1 : 
+				System.out.println("enter the name of file to be created");	
+				String filename;
+				filename =scanner.next() ;
 				filename = filename.concat(".txt");
 				System.out.println(filename);
-				
-				try(FileWriter fw=new FileWriter(filename);
+				File my_file_dir = new File("/Users/bijithmp/Desktop/phase_1/phase_1_file");
+				String pathnames[] = null;;
+				try {
+				pathnames = my_file_dir.list();
+				}catch (NullPointerException ex) {
+					System.out.println("directory is empty");
+				} 
+				Arrays.sort(pathnames);
+				int ch = Arrays.binarySearch(pathnames, filename);
+				if(ch>0)
+					System.out.println("please enter another name");
+				else {
+					   try(FileWriter fw=new FileWriter(filename);
+									
+									BufferedWriter bw=new BufferedWriter(fw);
+									){
+								System.out.println("your file created successfully");
+								System.out.println("enter the data to be written on file  ");
+								String inputString;
+								inputString = scanner.next();
+								 bw.write(inputString);
+								
+							
+							
+								System.out.println("Messages written to file successfully");
+								
+								
+								
+							}
+							catch (IOException e) {
+								System.out.println(e);
+							}
+					   
 						
-						BufferedWriter bw=new BufferedWriter(fw);
-						){
-					System.out.println("your file created successfully");
-					System.out.println("enter the data to be written on file");
-					String inputString = scanner. next();
-					
-					bw.write(inputString);
-				
-					System.out.println("Messages written to file successfully");
-					
-				} catch (IOException e) {
-					System.out.println(e);
 				}
+				   
+					
 				break;
+				  
+				
 				case 2 :System.out.println("enter the name of the file to be deleted");
 				
 				String fn = scanner.next();
@@ -71,18 +106,18 @@ import java.io.FileWriter;
 			
 				
 				case 3:System.out.println("display the files in ascending order");
-						File my_file_dir = new File("/Users/bijithmp/Desktop/phase_1/phase_1_file");
-						String pathnames [];
+						File my_file_dire = new File("/Users/bijithmp/Desktop/phase_1/phase_1_file");
+						String pathnames1 [];
 
-							if (my_file_dir.exists()) 
+							if (my_file_dire.exists()) 
 								{
 								
-								System.out.println("files"+my_file_dir.getName());
-								pathnames = my_file_dir.list();
-								Arrays.sort(pathnames);
+								System.out.println("files"+my_file_dire.getName());
+								pathnames1 = my_file_dire.list();
+								Arrays.sort(pathnames1);
 				   
 				   
-				   for (String pathname : pathnames) {
+				   for (String pathname : pathnames1) {
 					    // Print the names of files and directories
 					    System.out.println(pathname);
 					}
@@ -94,39 +129,39 @@ import java.io.FileWriter;
 							break;
 				case 4:System.out.println("enter the name of file you want to search?");
 						String search_key;
+						
+						
 						search_key = scanner.next();
 						search_key = search_key.concat(".txt");
+						
+						
 				File my_file_direc = new File("/Users/bijithmp/Desktop/phase_1/phase_1_file");
 				String path_name [];
 				
 				 
 				   if (my_file_direc.exists()) {
 					   path_name = my_file_direc.list();
-					   List<String> list = Arrays.asList(path_name);
-					  
-						for (Iterator iterator = list.iterator(); iterator.hasNext();) {
-							
-							
-						
+					   Arrays.sort(path_name);
+						int cha = Arrays.binarySearch(path_name, search_key);
+						if(cha>0) {
+							System.out.println("file found in the  directory .");
+							break;
+						}
+						else {
+							System.out.println("not found");
+							break;
+						}
 					
-					   if(list.contains(search_key)) {
-						   System.out.println("file you entered found in the directory");
-						   break;
-						   
-					   }
-					   else 
-						   System.out.println("file you entered not found in the directory ");
-					   		break;
-						    
-					   }
-				   
-				   
 				   }
 				  
 				
+				else {
+					System.out.println("no files in the directory.");
+				}
 
-				   
+				   break;  
 				case 0:System.out.println("Thank you for using our app!!");
+				break;
 				}
 				
 				
@@ -137,8 +172,7 @@ import java.io.FileWriter;
 			
 			
 			
-			
+		scanner.close();	
 		}
 
 	}
-	
